c670f3822b48fb529953f85bd87da5cc82528a2e
Brendan Mercer <bmercer3@student.cvtc.edu>
2026-02-25 16:33:33 -0600

    Resolve Ticket 501

--- src/edu/cvtc/itsd/Main.java
+++ src/edu/cvtc/itsd/Main.java
@@ -290,6 +290,12 @@ public class Main {
     labelState.setForeground(Color.magenta);
     panelStatus.add(labelState);
 
+    buttonAcknowledge = new JButton("OK");
+    buttonAcknowledge.addActionListener(handler);
+    buttonAcknowledge.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+    buttonAcknowledge.setForeground(Color.green);
+    panelStatus.add(buttonAcknowledge);
+
     panelStatus.add(Box.createVerticalGlue());
 
     // Error panel ////////////////////////////////////////////////////////////
